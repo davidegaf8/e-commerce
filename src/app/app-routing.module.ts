cc5a029b03d96494594a98d3d9a004c9720f8360
davidegaf8 <134536189+davidegaf8@users.noreply.github.com>
2024-04-04 17:48:31 +0200

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
 
@@ -8,7 +7,8 @@ import { CartComponent } from './components/cart/cart.component';
 const routes: Routes = [
   {path:'', redirectTo:'home', pathMatch:'full'},
   {path:'home', component:HomeComponent},
-  {path:'cart', component:CartComponent}
+  {path:'cart', component:CartComponent},
+  {path:'**', redirectTo:'home', pathMatch:'full'},//{path:'**', component:PageNotFoundComponent**}
 ];
 
 @NgModule({
